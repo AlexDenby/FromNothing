5066de3ccbf24a2b842cc0e558268bfb57512296
AleksDimitrov <adimitrov@email.arizona.edu>
2019-10-06 13:13:37 -0700

    Re Push Day 2.1
    Collide and Fade Repo

--- Assets/Scripts/KillTrigger.cs
+++ Assets/Scripts/KillTrigger.cs
@@ -5,20 +5,42 @@ using System.Collections;
 {
     public GameObject myPrefab;
     // Instantiate at position (0, 0, 0) and zero rotation.
+    public GameObject Progression;
     void OnTriggerEnter(Collider col)
     {
 
-        if (col.gameObject.name == "Player")
+        if (col.gameObject.tag == "Player")
         {
+            Progression.GetComponent<ProgressionHandler>().Kill();
             //For stopping the same object
             //GetComponent<PlayerController>().enabled = false;
-            //Pass through (destroy right now) object
+            //Destroy object
             //Destroy(col.gameObject);
             //For stopping other object
-            GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
+            //GameObject.Find("GameObject").GetComponent<ProgressionHandler>().enabled = true;
+            //Destroy(col.gameObject);
+            /*//For stopping other object
+            GameObject.Find("Player2").GetComponent<PlayerController>().enabled = false;
+            //Destroy object
+            Destroy(col.gameObject);
+            //For stopping other object
+            GameObject.Find("Player3").GetComponent<PlayerController>().enabled = false;
+            //Destroy object
+            Destroy(col.gameObject);
+            //For stopping other object
+            GameObject.Find("Player4").GetComponent<PlayerController>().enabled = false;
+            //Destroy object
+            Destroy(col.gameObject);
+            //For stopping other object
+            GameObject.Find("Player5").GetComponent<PlayerController>().enabled = false;
+            //Destroy object
+            Destroy(col.gameObject);
             GameObject.Find("Canvas").GetComponent<Fade5>().enabled = true;
+            */
             // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
-            Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            //Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+
         }      
 
     }
--- Assets/Scripts/ProgressionHandler.cs
+++ Assets/Scripts/ProgressionHandler.cs
@@ -9,11 +9,29 @@ public class ProgressionHandler : MonoBehaviour
     public GameObject[] Construction = new GameObject[10];
     public GameObject[] Lab = new GameObject[10];
     public GameObject[] OtherNPCs = new GameObject[10];
+    public GameObject FaceDeath;
+    public int i = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void Kill()
+    {
+        GameObject Player = Players[i];
+        Player.transform.GetChild(0).enabled = true;
+        transform.parent.GetChild(0).gameObject.SetActive(true);
+        //Player.transform.lookAt(FaceDeath);
+        Player.SetActive(false);
+
+
+
+
+
+        i++;
     }
 
     // Update is called once per frame
