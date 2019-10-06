7b651e044bc4cadeca2d806eb0d0bccd59680967
Unknown <jonatan@email.arizona.edu>
2019-10-05 17:21:22 -0700

    real shit title screen shit hell yeah

---
+++ Assets/Scripts/TitleTransition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleTransition : MonoBehaviour
+{
+    public bool fuckyeah = false;
+    public Vector3 pos;
+    public float speed;
+    public float bound;
+    public bool goingup;
+    public bool isSceneTrigger;
+
+    void Update() {
+        if (Input.anyKey) {
+            fuckyeah = true;
+        }
+        if (!goingup && fuckyeah && transform.position.y >= bound) {
+        	transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+        }
+        if (goingup && fuckyeah && transform.position.y <= bound) {
+        	transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+        }
+
+        if (isSceneTrigger && !goingup && transform.position.y <= bound) {
+			SceneManager.LoadScene("Level0");
+        } else if (isSceneTrigger && goingup && transform.position.y >= bound) {
+        	SceneManager.LoadScene("Level0");
+        }
+    }
+
+}
