c054eaeceee41fe25414b9a673864182ea26d788
Denbus <32473429+Denbus@users.noreply.github.com>
2019-10-06 13:48:48 -0700

    FUCK FUCK FUCK

--- Assets/Scripts/KillTrigger.cs
+++ Assets/Scripts/KillTrigger.cs
@@ -3,9 +3,8 @@ using System.Collections;
 
  public class KillTrigger : MonoBehaviour
 {
-    public GameObject myPrefab;
-    // Instantiate at position (0, 0, 0) and zero rotation.
-    public GameObject Progression;
+    private IEnumerator coroutine;
+
     void OnTriggerEnter(Collider col)
     {
 
@@ -41,8 +40,24 @@ using System.Collections;
             //Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
 
 
+            
+            coroutine = Wait();
+            StartCoroutine(coroutine);
+
+            GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
+            GameObject.Find("Canvas").GetComponent<Fade5>().enabled = false;
+
+            Transform pos = GameObject.Find("Player").GetComponent<Transform>();
+            Vector3 spawn = new Vector3(0, 11, -9);
+            pos.transform.position = spawn;
+
         }
 
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10);
     }
 }
 
