2920da99f96df07cce02fcc1c86ce94925feb036
Denbus <32473429+Denbus@users.noreply.github.com>
2019-10-05 22:12:42 -0700

    Player no longer forgets to walk while moving forwards

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,18 +20,23 @@ public class PlayerController : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+
+        float hmove = Input.GetAxis("Horizontal");
+        float vmove = Input.GetAxis("Vertical");
+
+        if (hmove != 0 || vmove != 0)
+        {
+            animator.SetBool("Walk", true);
+            Debug.Log("is moving");
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+            Debug.Log("is not moving");
+        }
     }
 
     void FixedUpdate() {
-        float move = Input.GetAxis ("Horizontal");
-
-        if ( move != 0 ) {
-            animator.SetBool( "Walk", true );
-            Debug.Log("is moving");
-        }
-        else {
-            animator.SetBool( "Walk", false );
-            Debug.Log("is not moving");
-        }
+        
     }
 }
