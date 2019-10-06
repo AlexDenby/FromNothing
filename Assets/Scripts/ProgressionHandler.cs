4537742d2305bc9ca9b977698ca464a3a47b216d
Denbus <32473429+Denbus@users.noreply.github.com>
2019-10-06 13:18:38 -0700

    AAAAAHHHH

--- Assets/Scripts/ProgressionHandler.cs
+++ Assets/Scripts/ProgressionHandler.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class ProgressionHandler : MonoBehaviour
 {
-    public GameObject[] Players = new GameObject[10];
+    public GameObject player;
     public GameObject[] PettingZoo = new GameObject[10];
     public GameObject[] Construction = new GameObject[10];
     public GameObject[] Lab = new GameObject[10];
     public GameObject[] OtherNPCs = new GameObject[10];
     public GameObject FaceDeath;
-    public int i = 0;
 
 
     // Start is called before the first frame update
@@ -21,17 +20,15 @@ public class ProgressionHandler : MonoBehaviour
 
     public void Kill()
     {
-        GameObject Player = Players[i];
+        
         //Player.transform.GetChild(0).enabled = true;
         //transform.parent.GetChild(0).gameObject.SetActive(true);
-        //Player.transform.lookAt(FaceDeath);
-        Player.SetActive(false);
+        //transform.lookAt(FaceDeath);
 
 
 
 
 
-        i++;
     }
 
     // Update is called once per frame
