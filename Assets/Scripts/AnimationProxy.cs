4dfa9c3361fd6373db12ad75c571c85c3dce0f80
Unknown <jonatan@email.arizona.edu>
2019-10-05 22:03:19 -0700

    animation fuck

---
+++ Assets/Scripts/AnimationProxy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationProxy : MonoBehaviour
+{
+    public Animator animator;
+    public bool move;
+
+    void Update() {
+    	
+    	if (move) {
+    		animator.SetBool( "Walk", true );
+    	} else {
+    		animator.SetBool( "Walk", false );
+    	}
+    	
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 3.0F;
     public float rotateSpeed = 3.0F;
 
+    public Animator animator;
+
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
@@ -18,5 +20,18 @@ public class PlayerController : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+    }
+
+    void FixedUpdate() {
+        float move = Input.GetAxis ("Horizontal");
+
+        if ( move != 0 ) {
+            animator.SetBool( "Walk", true );
+            Debug.Log("is moving");
+        }
+        else {
+            animator.SetBool( "Walk", false );
+            Debug.Log("is not moving");
+        }
     }
 }
